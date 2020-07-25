3c129ccbd1ca36d6da702df322f05008423edc02
Todd Christensen <todd@Todds-MacBook-Pro.local>
2020-07-25 17:09:46 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,23 +9,32 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'christyvault.net',
+        baseUrl: 'http://35.245.19.7',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: 'https',
+        protocol: 'http',
         // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: false,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
+        includedRoutes: [
+          '**/categories',
+          '**/posts',
+          '**/pages',
+          '**/media',
+          '**/tags',
+          '**/taxonomies',
+          '**/users',
+        ],
       },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
       // Removes unused css rules
-      resolve:'gatsby-plugin-purgecss',
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         // Activates purging in gatsby develop
         develop: true,
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,9 @@ import Helmet from 'react-helmet'
 import Navbar from './Navbar'
 import './all.sass'
 
+import '../assets/css/elementor/frontend.min.css'
+import '../assets/css/elementor/common.min.css'
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + WordPress" />
